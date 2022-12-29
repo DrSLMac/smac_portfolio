4b3c3357c3c55b27f05e16ec7eebecdf2d60350a
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-12-29 02:28:57 -0600

    responsive contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -26,52 +26,68 @@ const ContactMe = ({ pageInfo }: Props) => {
     <div 
         className='relative h-screen flex flex-col text-center  md:flex-row  md:px-10 px-2 justify-evenly items-center'
     >
-        <h3 className='absolute top-20 md:top-24 uppercase tracking-[20px] text-[#44615E] text-2xl'>
+        <h3 className='absolute uppercase text-[#44615E]
+            top-[50px] tracking-[15px] text-xl
+            md:top-20 md:tracking-[20px] md:text-2xl
+            lg:text-3xl lg:tracking-[22px]'>
             Contact
         </h3>
 
         <img 
-            className='absolute top-[70px] right-[18px] h-[45px] md:h-[150px] md:right-[40px] md:top-[120px] xl:h-[250px] xl:right-[100px]'
+            className='absolute 
+                top-[130px] right-[22px] h-[45px] 
+                md:h-[150px] md:right-[40px] md:top-[120px] 
+                xl:h-[250px] xl:right-[100px]'
             src={urlFor(pageInfo.design3).url()}
             alt="abstract art"
         />
         <img 
-            className='absolute -rotate-12 h-[110px] left-[15px] top-[180px] md:h-[250px] md:left-[30px] md:top-[300px] xl:h-[470px] xl:left-[80px] xl:top-[180px]'
+            className='absolute -rotate-12 z-0
+                h-[90px] left-[12px] top-[100px] 
+                md:h-[250px] md:left-[30px] md:top-[300px] 
+                xl:h-[470px] xl:left-[80px] xl:top-[180px]'
             src={urlFor(pageInfo.design4).url()}
             alt="abstract art"
         />
            <img 
-            className='absolute h-[70px] bottom-8 right-8 md:h-[160px] md:right-[40px] md:bottom-[50px] xl:h-[260px] xl:right-[100px]'
+            className='absolute
+                h-[50px] bottom-5 right-8 
+                md:h-[160px] md:right-[40px] md:bottom-[50px] 
+                xl:h-[260px] xl:right-[100px]'
             src={urlFor(pageInfo.design5).url()}
             alt="abstract art"
         />
 
-        <div className='flex flex-col space-x-6 space-y-10 w-screen items-center mx-4 z-50'>
-            <h4 className='text-2xl md:text-4xl font-semibold text-center pt-10'>
-                I have got just what you need.{" "}
+        <div className='flex flex-col items-center z-50
+             space-y-4'>
+            <h4 className='text-lg md:text-4xl font-semibold text-center'>
+                Ready to build something beautiful?{" "}
                 <br></br>
-                <span className='decoration-[#CE928C] underline'>Ready to talk?</span>
+                <span className='decoration-[#CE928C] underline'>Drop me a line!</span>
             </h4>
 
-            <div className='space-y-10'>
+            <div className='space-y-1 text-base md:text-2xl'>
                 <div className='flex items-center md:space-x-5 space-x-2 justify-center'>
                     <PhoneIcon className="text-[#44615E] h-5 md:h-7 md:w-7 animate-pulse"/>
-                    <p className='text-xl md:text-2xl'>+1-817-718-7210</p>
+                    <p>+1-817-718-7210</p>
                 </div>
 
                 <div className='flex items-center md:space-x-5 space-x-1 justify-center'>
                     <EnvelopeIcon className="text-[#44615E] h-5 md:h-7 md:w-7 animate-pulse"/>
-                    <p className='text-xl md:text-2xl'>sageheartdesigns@gmail.com</p>
+                    <p>sageheartdesigns@gmail.com</p>
                 </div>
 
                 <div className='flex items-center md:space-x-5 space-x-1 justify-center'>
                     <MapPinIcon className="text-[#44615E] h-7 w-7 animate-pulse"/>
-                    <p className='text-xl md:text-2xl'>Fort Worth, TX</p>
+                    <p>Fort Worth, TX</p>
                 </div>
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-auto mx-auto'>
-                <div className='flex space-x-2'>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-auto mx-auto
+            text-sm'>
+                <div className='flex 
+                    flex-col space-y-2 w-80 text-sm
+                    md:flex-row'>
                     <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
                     <input {...register('email')} placeholder='Email' className='contactInput' type="email" />
                 </div>
@@ -79,7 +95,12 @@ const ContactMe = ({ pageInfo }: Props) => {
                 <input {...register('subject')}placeholder='Subject' className='contactInput' type="text"/>
 
                 <textarea {...register('message')} placeholder='Message' className='contactInput' />
-                <button className='bg-[#44615E] py-5 px-10 rounded-md text-[#CE928C] font-bold text-xl'>Submit</button>
+                <button className='bg-[#44615E] rounded-md text-[#CE928C]
+                    py-3 px-10 font-bold uppercase
+                    text-lg tracking-[4px] '
+                >
+                    Submit
+                </button>
             </form>
         </div>
     </div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,17 +14,29 @@ const Projects = ({ projects }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='relative h-screen flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
+      className='relative h-screen flex flex-col justify-evenly items-center overflow-hidden z-0
+        mx-auto
+        md:flex-row 
+        max-w-full'
     >
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-[#44615E] text-2xl'>
+      <h3 className='absolute uppercase text-[#44615E]
+            top-[50px] tracking-[15px] text-xl
+            md:top-20 md:tracking-[20px] md:text-2xl
+            lg:text-3xl lg:tracking-[22px]'>
         Projects
       </h3>
 
-      <div className='relative w-full h-[1000px] flex overflow-x-scroll  snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-[#EADFD6] scrollbar-thumb-[#997C80]/80'>
+      <div className='relative flex overflow-x-scroll  
+        snap-x snap-mandatory z-20 
+        scrollbar-thin scrollbar-track-[#EADFD6] scrollbar-thumb-[#997C80]/80
+        w-full h-[900px] mt-10'>
         {projects.map((project, i) => (
           <motion.div 
             key={Math.random()} 
-            className='w-screen flex-shrink-0 snap-center flex flex-col space-y-1 md:space-y-5 items-center justify-center px-2 md:p-44 '
+            className='flex flex-col flex-shrink-0 items-center justify-center
+              snap-center mt-5
+              w-screen space-y-1 px-3
+              md:space-y-5  md:p-44 '
           >
             <Link key={Math.random()} href={project.linkToBuild}>
               <motion.img 
@@ -35,19 +47,30 @@ const Projects = ({ projects }: Props) => {
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 src={urlFor(project.image).url()}
-                className="h-[180px] md:h-80 rounded-lg drop-shadow-xl"
+                className="rounded-lg drop-shadow-xl
+                  h-[190px] 
+                  md:h-80"
               />
             </Link>
 
-            <div className='space-y-2 md:space-y-4 px-0 md:px-6 max-w-6xl'>
-              <h4 className='text-xl md:text-4xl font-semibold text-center'>
+            <div className='
+              space-y-2
+              md:space-y-4 md:px-6 
+              max-w-6xl'>
+              <h4 className='font-semibold text-center
+                text-xl mt-3
+                md:text-4xl'>
                 <span className='underline decoration-[#CE928C]'>
                   Project {i + 1} of {projects.length}:
                 </span>{"  "}
                 {project.title}
               </h4>
 
-              <div className='text-sm md:text-lg text-left px-4 h-[300px] md:h-[350px] md:w-[700px] xl:w-[900px] overflow-y-scroll '>
+              <div className='text-left overflow-y-scroll
+                scrollbar-thin scrollbar-track-[#EADFD6] scrollbar-thumb-[#997C80]/80
+                text-sm px-4 h-[275px]
+                md:text-lg md:h-[350px] md:w-[700px] 
+                xl:w-[900px]'>
                 <p>
                   {project.summary}
                 </p>
